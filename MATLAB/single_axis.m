1450764a8ca540826c5843c40026689f9f579645
GiacomoCorradini <corra981@gmail.com>
2022-04-27 13:16:24 +0200

    update lesson 27/04/2022

---
+++ MATLAB/single_axis.m
@@ -0,0 +1,28 @@
+%% single axis example
+
+clc
+clear all
+close all
+
+%% Parameters
+
+% base
+lb = 500; % mm
+wb = 25; % mm
+tb = 15; % mm
+
+% cursor
+t = tb;
+w = wb;
+l = w;
+
+%% Motor torque curve
+
+% rotational vector
+w_m = [0, 1000, 2000, 3000];
+
+% torque of the motor
+t_m = [1, 1, 0.95, 0] * 200;
+
+figure
+plot(w_m, t_w)
